6474201975f1430429588a577a25435e34c2efb6
MrUnknowner <nobodymdbot@gmail.com>
2026-09-01 19:12:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 8000;
-const pairRoute = require("./pair");
+const code = require("./pair");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/code", pairRoute);
+app.use("/code", code);
 
-app.use("/", (req, res) => {
+app.use("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "pair.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 MrNobody Pair Server running on port ${PORT}`);
+  console.log(`⏩ Server running on port ${PORT}`);
 });
 
 module.exports = app;
