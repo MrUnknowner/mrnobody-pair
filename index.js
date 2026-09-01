18d061f036934fac70a83023c8bdee2f749b53dd
MrUnknowner <nobodymdbot@gmail.com>
2026-09-01 18:33:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const __path = process.cwd();
-const PORT = process.env.PORT || 3000;
-const code = require("./pair");
+const path = require("path");
+const PORT = process.env.PORT || 8000;
+let code = require("./pair");
+
+require("events").EventEmitter.defaultMaxListeners = 500;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/code", code);
 
 app.use("/", async (req, res) => {
-  res.sendFile(__path + "/pair.html");
+  res.sendFile(path.join(__dirname, "pair.html"));
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`⏩ Server running on port ${PORT}`);
 });
 
 module.exports = app;
