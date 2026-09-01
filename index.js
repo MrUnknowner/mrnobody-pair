6984237024da2e23c6c018d6a1ffea20d7d9cd33
MrUnknowner <nobodymdbot@gmail.com>
2026-09-01 18:11:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const pairRouter = require('./pair');
-
-const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: '*' }));
-app.use(express.json());
+const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send("🚀 MrNobody Base64 Pair Server is Live!");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Zanta එකේ වගේම /pair ගියපු ගමන් pair.html එක පේන්න
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
 });
 
-app.use('/pair', pairRouter);
+// Pair route එක
+const pairRouter = require('./pair');
+app.use('/code', pairRouter);
 
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`MrNobody Server running on port ${PORT}`);
+});
