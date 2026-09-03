0233da408ed7f89f9eb0fd35a8eae08e32cda91e
MrUnknowner <nobodymdbot@gmail.com>
2026-09-03 10:16:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express();
 __path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
+
 let code = require("./pair");
+let qr = require("./qr"); // අලුත් QR ෆයිල් එක
+
 require("events").EventEmitter.defaultMaxListeners = 500;
+
 app.use("/code", code);
+app.use("/qr", qr); // අලුත් QR route එක
 
 app.use("/", async (req, res, next) => {
   res.sendFile(__path + "/pair.html");
@@ -13,6 +18,7 @@ app.use("/", async (req, res, next) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log(`⏩ Server running on http://localhost:` + PORT);
 });
